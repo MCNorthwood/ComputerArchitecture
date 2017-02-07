5251a9878234f25811673d27deb6bd1c8f10d297
MCNorthwood <HotBlockMC@gmail.com>
2017-02-07 15:59:41 +0000

    First Prototype

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float startSpeed = 10f;
+
+    [HideInInspector]
+    public float speed;
+
+	void Start ()
+    {
+        speed = startSpeed;
+	}
+}
